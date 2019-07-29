fa42a98849531b7f7bb134a071804198ffdb2ef4
wangsc76 <53336161+wangsc76@users.noreply.github.com>
2019-07-29 08:40:43 +0800

    Update and rename src/com/czxy/test/Test.java to  src/ com/ czxy/ test/Test.java

---
+++ src/ com/ czxy/ test/Test.java
@@ -0,0 +1,15 @@
+package com.czxy.test;
+
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/27
+ */
+public class Test {
+    public static void main(String[] args) {
+        系统。出。println（“测试03动作”）；
+        系统。出。println（“测试03动作”）；
+        系统。出。println（“测试03动作”）；
+            
+
+}
