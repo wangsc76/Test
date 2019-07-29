2c80169b99b3a4e5d49a727893a4c35e8ed4e359
kuangwei <19976139162@qq.com>
2019-07-29 08:36:44 +0800

    提交测试03至Git

---
+++ src/com/czxy/test/Test.java
@@ -0,0 +1,12 @@
+package com.czxy.test;
+
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/27
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试03 action");
+    }
+}
---
+++ src/com/czxy/test/Test02.java
@@ -0,0 +1,13 @@
+package com.czxy.test;
+
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/27
+ */
+public class Test02 {
+
+    public static void main(String[] args) {
+        System.out.println("测试03 test02");
+    }
+}
